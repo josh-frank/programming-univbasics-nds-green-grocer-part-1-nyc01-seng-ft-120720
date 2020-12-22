ffa44dd401eb46299db675b5443a927c1dc3d1e5
Joshua Frank <eurosewusa@gmail.com>
2020-12-22 01:54:56 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,7 +1,4 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
   collection.find{ | collected | collected[ :item ] == name  }
 end
 
